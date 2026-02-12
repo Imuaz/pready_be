ed276069b241eae9462c4fdcbba5f8429162455a
iMuaz <imuazyakub@gmail.com>
2026-02-12 21:43:17 +0100

    feat: implement email services for user verification, password reset, and password change notifications.

---
+++ src/config/email.ts
@@ -0,0 +1,44 @@
+import nodemailer, { Transporter } from "nodemailer";
+import type { EmailConfig } from "@/types/type.js";
+
+
+/**
+ * Create email transporter based on environment
+ */
+const createTransporter = (): Transporter => {
+  const config: EmailConfig = {
+    host: process.env.EMAIL_HOST || 'sandbox.smtp.mailtrap.io',
+    port: parseInt(process.env.EMAIL_PORT || '2525', 10),
+    auth: {
+      user: process.env.EMAIL_USER || '',
+      pass: process.env.EMAIL_PASS || ''
+    }
+  };
+
+  // For production, we may use different config
+  // if (process.env.NODE_ENV === 'production') {
+  //   config = {
+  //     service: 'SendGrid',
+  //     auth: {
+  //       user: 'apikey',
+  //       pass: process.env.SENDGRID_API_KEY || ''
+  //     }
+  //   };
+  // }
+
+  const transporter = nodemailer.createTransport(config);
+
+  // Verify connection configuration
+  transporter.verify((error, success) => {
+    if (error) {
+      console.error('❌ Email configuration error:', error);
+    } else {
+        console.log(success)
+        console.log('✅ Email server is ready to send messages');
+    }
+  });
+
+  return transporter;
+};
+
+export default createTransporter;
